25c39ada9fb95e3805b453b2c3758e823cf006bb
Stuart Leal <stuart.leal23@gmail.com>
2017-11-06 08:11:30 -0600

    Implementacion de initialize e insert

--- B53777_tarea5_grupo1.s
+++ B53777_tarea5_grupo1.s
@@ -14,8 +14,10 @@ main:
 
 	# Probar insert
 	# Cargar numero
-	li $t0, 10
-	swc1 $f0, 0($t0)
+	li.s $f0, 10.0
+	jal insert
+
+	li.s $f0, 20.0
 	jal insert
 	
 	# Devolver cola y devolverse
@@ -38,7 +40,11 @@ insert:
 	# Recibe en a0 ptr list
 	# Recibe en f0 dato
 	# Devuelve en v0 1 si fue exitoso
-
+	
+	# Guardar en la pila a0
+	addi $sp, $sp, -4
+	sw $a0, 0($sp)
+	
 	# Guardar direccion de ptrList
 	add $t0, $0, $a0
 
@@ -47,6 +53,36 @@ insert:
 	li $v0, 9
 	syscall
 
+	# Ahora en v0 esta la direccion
+	s.d $f0, 0($v0)
+
+	# Encontrar la direccion de memoria
+	# Primero guardo un contador 	
+	addi $t1, $0, 0
+
+	# Guardo a0 en t1 para iterar
+	add $t1, $t1, $t0
+
+find_last:
+	# Cargar el dato de la posicion
+	lw $t2, 0($t1)
+	beq $t2, $0, encontrado
+
+	# Si no es cero, tiene una direccion
+	add $t1, $t2, $0
+
+	# Volver al loop
+	j find_last
+	
+encontrado:	
+	# Guardar la direccion en el ultimo puntero
+	addi $v0, $v0, 4 # mover a la palabra del puntero
+	sw $v0, 0($t1)
+
+	# Devolver cola y devolverse
+	lw $a0, 0($sp)
+	addi $sp, $sp, 4
+	
 	# Regresar
 	jr $ra
 	
